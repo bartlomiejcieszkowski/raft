bfc7fb81c6d5538f4157ece643432c38a94a8e25
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2016-06-07 22:28:28 +0200

    Initial commit listing all local branches as directories

--- Gafa/Dokan/CustomFilesystem.cs
+++ Gafa/Dokan/CustomFilesystem.cs
@@ -6,129 +6,527 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Security.AccessControl;
+using System.Threading;
+using LibGit2Sharp;
 
 namespace Gafa.Dokan
 {
-    class CustomFilesystem : IDokanOperations
-    {
-        public void Cleanup(string fileName, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+	public class FolderEntry
+	{
+		public string Mountpoint;
+		public string Folderpath;
+	}
 
-        public void CloseFile(string fileName, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+	public class Singleton<U> where U : class, new()
+	{
+		private static U _instance;
 
-        public NtStatus CreateFile(string fileName, DokanNet.FileAccess access, FileShare share, FileMode mode, FileOptions options, FileAttributes attributes, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+		public static U Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					lock (safetyLock)
+					{
+						if (_instance == null)
+						{
+							_instance = new U();
+						}
+					}
+				}
+				return _instance;
+			}
+		}
 
-        public NtStatus DeleteDirectory(string fileName, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+		protected Singleton() { }
 
-        public NtStatus DeleteFile(string fileName, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+		private static object safetyLock = new object();
+	}
 
-        public NtStatus FindFiles(string fileName, out IList<FileInformation> files, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+	public class MountsList : Singleton<List<IFileSystem>>
+	{
+		private MountsList() : base() { }
 
-        public NtStatus FindStreams(string fileName, out IList<FileInformation> streams, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+		public static void Mount()
+		{
+			foreach(var entry in Instance)
+			{
+				entry.Mount();
+			}
+		}
 
-        public NtStatus FlushFileBuffers(string fileName, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+		public static void Unmount()
+		{
+			foreach(var entry in Instance)
+			{
+				entry.Unmount();
+			}
+		}
+	}
 
-        public NtStatus GetDiskFreeSpace(out long freeBytesAvailable, out long totalNumberOfBytes, out long totalNumberOfFreeBytes, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+	public interface IFileSystem
+	{
+		void Mount();
+		void Unmount();
+	}
 
-        public NtStatus GetFileInformation(string fileName, out FileInformation fileInfo, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+	public class FilesystemStringOperations
+	{
+		public static bool IsRoot(string fileName)
+		{
+			return fileName == @"\";
+		}
+	}
 
-        public NtStatus GetFileSecurity(string fileName, out FileSystemSecurity security, AccessControlSections sections, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+	public class FilesystemInformation
+	{
+		public DateTime OpenTime;
+		public string VolumeLabel;
+		public string Mountpoint;
+	}
 
-        public NtStatus GetVolumeInformation(out string volumeLabel, out FileSystemFeatures features, out string fileSystemName, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+	public partial class GitFilesystem : IFileSystem, IDokanOperations
+	{
+		FilesystemInformation FilesystemInformation;
+		string RepositoryPath;
 
-        public NtStatus LockFile(string fileName, long offset, long length, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+		public GitFilesystem(string repositoryPath, string mountPoint)
+		{
+			FilesystemInformation = new FilesystemInformation()
+			{
+				VolumeLabel = Path.GetDirectoryName(repositoryPath),
+				Mountpoint = mountPoint,
+				OpenTime = DateTime.UtcNow
+			};
+			RepositoryPath = repositoryPath;
+		}
 
-        public NtStatus Mounted(DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+		public void Mount()
+		{
+			new Thread(() =>
+			{
+				DokanNet.Dokan.Mount(this, FilesystemInformation.Mountpoint);
+			}).Start();
+		}
 
-        public NtStatus MoveFile(string oldName, string newName, bool replace, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+		public void Unmount()
+		{
+			new Thread(() =>
+			{
+				DokanNet.Dokan.RemoveMountPoint(FilesystemInformation.Mountpoint);
+			}).Start();
+		}
+	}
 
-        public NtStatus ReadFile(string fileName, byte[] buffer, out int bytesRead, long offset, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+	public partial class GitFilesystem : IFileSystem, IDokanOperations
+	{
+		public NtStatus Mounted(DokanFileInfo info)
+		{
+			return DokanResult.Success;
+		}
 
-        public NtStatus SetAllocationSize(string fileName, long length, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+		public NtStatus Unmounted(DokanFileInfo info)
+		{
+			return DokanResult.Success;
+		}
 
-        public NtStatus SetEndOfFile(string fileName, long length, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+		public NtStatus GetFileInformation(string fileName, out FileInformation fileInfo, DokanFileInfo info)
+		{
+			fileInfo = new FileInformation();
+			fileInfo.FileName = fileName;
 
-        public NtStatus SetFileAttributes(string fileName, FileAttributes attributes, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+			if (FilesystemStringOperations.IsRoot(fileName))
+			{
 
-        public NtStatus SetFileSecurity(string fileName, FileSystemSecurity security, AccessControlSections sections, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+				fileInfo.Attributes = FileAttributes.Directory;
+				fileInfo.LastAccessTime = FilesystemInformation.OpenTime;
+				fileInfo.LastWriteTime = FilesystemInformation.OpenTime;
+				fileInfo.CreationTime = FilesystemInformation.OpenTime;
+				return DokanResult.Success;
+			}
 
-        public NtStatus SetFileTime(string fileName, DateTime? creationTime, DateTime? lastAccessTime, DateTime? lastWriteTime, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+			//var fileNameWithPath = Path.Combine(Folderpath, fileName);
+			//if (!File.Exists(fileNameWithPath))
+			//{
+			//	return DokanResult.Error;
+			//}
 
-        public NtStatus UnlockFile(string fileName, long offset, long length, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+			//var fileInfoI = new FileInfo(fileNameWithPath);
 
-        public NtStatus Unmounted(DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
+			//fileInfo.Attributes = fileInfoI.Attributes;
+			//fileInfo.LastAccessTime = fileInfoI.LastAccessTimeUtc;
+			//fileInfo.LastWriteTime = fileInfoI.LastWriteTimeUtc;
+			//fileInfo.LastAccessTime = fileInfoI.LastAccessTimeUtc;
+			//fileInfo.Length = fileInfoI.Length;
+			return DokanResult.Success;
+		}
 
-        public NtStatus WriteFile(string fileName, byte[] buffer, out int bytesWritten, long offset, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
-    }
+
+		public void Cleanup(string fileName, DokanFileInfo info)
+		{
+		}
+
+		public void CloseFile(string fileName, DokanFileInfo info)
+		{
+		}
+
+		public NtStatus CreateFile(string fileName, DokanNet.FileAccess access, FileShare share, FileMode mode, FileOptions options, FileAttributes attributes, DokanFileInfo info)
+		{
+			if (info.IsDirectory && mode == System.IO.FileMode.CreateNew)
+			{
+				return DokanResult.AccessDenied;
+			}
+			return DokanResult.Success;
+		}
+
+		public NtStatus DeleteDirectory(string fileName, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus DeleteFile(string fileName, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus FindFiles(string fileName, out IList<FileInformation> files, DokanFileInfo info)
+		{
+
+			files = new List<FileInformation>();
+
+
+			if(FilesystemStringOperations.IsRoot(fileName))
+			{
+				using (var repo = new Repository(RepositoryPath))
+				{
+					foreach(var branch in repo.Branches.Where(b => !b.IsRemote))
+					{
+						FileInformation branchInformation = new FileInformation()
+						{
+							Attributes = FileAttributes.Directory,
+							LastWriteTime = branch.Tip.Committer.When.UtcDateTime,
+							LastAccessTime = branch.Tip.Committer.When.UtcDateTime,
+							CreationTime = branch.Tip.Committer.When.UtcDateTime,
+							FileName = branch.FriendlyName
+						};
+						files.Add(branchInformation);
+					}
+				}
+			}
+			return DokanResult.Success;
+		}
+
+		public NtStatus FindStreams(string fileName, out IList<FileInformation> streams, DokanFileInfo info)
+		{
+			streams = new FileInformation[0];
+			return DokanResult.NotImplemented;
+		}
+
+		public NtStatus FlushFileBuffers(string fileName, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus GetDiskFreeSpace(out long freeBytesAvailable, out long totalNumberOfBytes, out long totalNumberOfFreeBytes, DokanFileInfo info)
+		{
+			totalNumberOfBytes = 1024 * 1024 * 1024;
+			freeBytesAvailable = totalNumberOfBytes / 2;
+			totalNumberOfFreeBytes = freeBytesAvailable;
+			return DokanResult.Success;
+		}
+
+
+
+		public NtStatus GetFileSecurity(string fileName, out FileSystemSecurity security, AccessControlSections sections, DokanFileInfo info)
+		{
+			security = null;
+			return DokanResult.Error;
+		}
+
+		public NtStatus GetVolumeInformation(out string volumeLabel, out FileSystemFeatures features, out string fileSystemName, DokanFileInfo info)
+		{
+			volumeLabel = "RFS";
+			features = FileSystemFeatures.None;
+			fileSystemName = String.Empty;
+			return DokanResult.Success;
+		}
+
+		public NtStatus LockFile(string fileName, long offset, long length, DokanFileInfo info)
+		{
+			return DokanResult.Success;
+		}
+
+		public NtStatus MoveFile(string oldName, string newName, bool replace, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus ReadFile(string fileName, byte[] buffer, out int bytesRead, long offset, DokanFileInfo info)
+		{
+			bytesRead = 0;
+			return DokanResult.Error;
+		}
+
+		public NtStatus SetAllocationSize(string fileName, long length, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus SetEndOfFile(string fileName, long length, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus SetFileAttributes(string fileName, FileAttributes attributes, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus SetFileSecurity(string fileName, FileSystemSecurity security, AccessControlSections sections, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus SetFileTime(string fileName, DateTime? creationTime, DateTime? lastAccessTime, DateTime? lastWriteTime, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus UnlockFile(string fileName, long offset, long length, DokanFileInfo info)
+		{
+			return DokanResult.Success;
+		}
+
+		public NtStatus WriteFile(string fileName, byte[] buffer, out int bytesWritten, long offset, DokanFileInfo info)
+		{
+			bytesWritten = 0;
+			return DokanResult.Error;
+		}
+	}
+
+	public class CustomFilesystem : FolderEntry, IFileSystem, IDokanOperations
+	{
+		DateTime OpenTime;
+		string VolumeLabel = "GAFA";
+
+		public void Mount()
+		{
+			new Thread(() =>
+			{
+				DokanNet.Dokan.Mount(this, GetMountpoint());
+			}).Start();
+		}
+
+		public void Unmount()
+		{
+			new Thread(() =>
+			{
+				DokanNet.Dokan.RemoveMountPoint(GetMountpoint());
+			}).Start();
+		}
+
+		public string GetMountpoint() { return Mountpoint; }
+
+
+		public CustomFilesystem(string mountpoint, string path) : base()
+		{
+			OpenTime = DateTime.UtcNow;
+			Mountpoint = mountpoint;
+			Folderpath = path;
+		}
+
+		public NtStatus Mounted(DokanFileInfo info)
+		{
+			OpenTime = DateTime.UtcNow;
+			return DokanResult.Success;
+		}
+
+		public NtStatus Unmounted(DokanFileInfo info)
+		{
+			return DokanResult.Success;
+		}
+
+		public NtStatus GetFileInformation(string fileName, out FileInformation fileInfo, DokanFileInfo info)
+		{
+			fileInfo = new FileInformation();
+			fileInfo.FileName = fileName;
+
+			if(FilesystemStringOperations.IsRoot(fileName))
+			{
+
+				fileInfo.Attributes = FileAttributes.Directory;
+				fileInfo.LastAccessTime = OpenTime;
+				fileInfo.LastWriteTime = OpenTime;
+				fileInfo.CreationTime = OpenTime;
+				return DokanResult.Success;
+			}
+
+			var fileNameWithPath =  Path.Combine(Folderpath, fileName);
+			if(!File.Exists(fileNameWithPath))
+			{
+				return DokanResult.Error;
+			}
+
+			var fileInfoI = new FileInfo(fileNameWithPath);
+
+			fileInfo.Attributes = fileInfoI.Attributes;
+			fileInfo.LastAccessTime = fileInfoI.LastAccessTimeUtc;
+			fileInfo.LastWriteTime = fileInfoI.LastWriteTimeUtc;
+			fileInfo.LastAccessTime = fileInfoI.LastAccessTimeUtc;
+			fileInfo.Length = fileInfoI.Length;
+			return DokanResult.Success;
+		}
+
+
+		public void Cleanup(string fileName, DokanFileInfo info)
+		{
+		}
+
+		public void CloseFile(string fileName, DokanFileInfo info)
+		{
+		}
+
+		public NtStatus CreateFile(string fileName, DokanNet.FileAccess access, FileShare share, FileMode mode, FileOptions options, FileAttributes attributes, DokanFileInfo info)
+		{
+			if(info.IsDirectory && mode == System.IO.FileMode.CreateNew)
+			{
+				return DokanResult.AccessDenied;
+			}
+			return DokanResult.Success;
+		}
+
+		public NtStatus DeleteDirectory(string fileName, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus DeleteFile(string fileName, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus FindFiles(string fileName, out IList<FileInformation> files, DokanFileInfo info)
+		{
+			
+			files = new List<FileInformation>();
+			DirectoryInfo dirInfo = new DirectoryInfo(Path.Combine(Folderpath, fileName.TrimStart('\\')));
+			//if(fileName == @"\")
+			//{
+
+			foreach (var directoryInfo in dirInfo.GetDirectories())
+			{
+				FileInformation fileInfoD = new FileInformation();
+				fileInfoD.FileName = directoryInfo.Name;
+				fileInfoD.Attributes = directoryInfo.Attributes;
+				fileInfoD.LastAccessTime = directoryInfo.LastAccessTimeUtc;
+				fileInfoD.LastWriteTime = directoryInfo.LastWriteTimeUtc;
+				fileInfoD.CreationTime = directoryInfo.CreationTimeUtc;
+				files.Add(fileInfoD);
+
+			}
+
+			foreach (var fileinfo in dirInfo.GetFiles())
+			{
+				FileInformation fileInfoD = new FileInformation();
+				fileInfoD.FileName = fileinfo.Name;
+				fileInfoD.Attributes = fileinfo.Attributes;
+				fileInfoD.LastAccessTime = fileinfo.LastAccessTimeUtc;
+				fileInfoD.LastWriteTime = fileinfo.LastWriteTimeUtc;
+				fileInfoD.CreationTime = fileinfo.CreationTimeUtc;
+				fileInfoD.Length = fileinfo.Length;
+				files.Add(fileInfoD);
+			}
+			//}
+
+			return DokanResult.Success;
+		}
+
+		public NtStatus FindStreams(string fileName, out IList<FileInformation> streams, DokanFileInfo info)
+		{
+			streams = new FileInformation[0];
+			return DokanResult.NotImplemented;
+		}
+
+		public NtStatus FlushFileBuffers(string fileName, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus GetDiskFreeSpace(out long freeBytesAvailable, out long totalNumberOfBytes, out long totalNumberOfFreeBytes, DokanFileInfo info)
+		{
+			totalNumberOfBytes = 1024 * 1024 * 1024;
+			freeBytesAvailable = totalNumberOfBytes / 2;
+			totalNumberOfFreeBytes = freeBytesAvailable;
+			return DokanResult.Success;
+		}
+
+
+
+		public NtStatus GetFileSecurity(string fileName, out FileSystemSecurity security, AccessControlSections sections, DokanFileInfo info)
+		{
+			security = null;
+			return DokanResult.Error;
+		}
+
+		public NtStatus GetVolumeInformation(out string volumeLabel, out FileSystemFeatures features, out string fileSystemName, DokanFileInfo info)
+		{
+			volumeLabel = "RFS";
+			features = FileSystemFeatures.None;
+			fileSystemName = String.Empty;
+			return DokanResult.Success;
+		}
+
+		public NtStatus LockFile(string fileName, long offset, long length, DokanFileInfo info)
+		{
+			return DokanResult.Success;
+		}
+
+		public NtStatus MoveFile(string oldName, string newName, bool replace, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus ReadFile(string fileName, byte[] buffer, out int bytesRead, long offset, DokanFileInfo info)
+		{
+			bytesRead = 0;
+			return DokanResult.Error;
+		}
+
+		public NtStatus SetAllocationSize(string fileName, long length, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus SetEndOfFile(string fileName, long length, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus SetFileAttributes(string fileName, FileAttributes attributes, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus SetFileSecurity(string fileName, FileSystemSecurity security, AccessControlSections sections, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus SetFileTime(string fileName, DateTime? creationTime, DateTime? lastAccessTime, DateTime? lastWriteTime, DokanFileInfo info)
+		{
+			return DokanResult.Error;
+		}
+
+		public NtStatus UnlockFile(string fileName, long offset, long length, DokanFileInfo info)
+		{
+			return DokanResult.Success;
+		}
+
+		public NtStatus WriteFile(string fileName, byte[] buffer, out int bytesWritten, long offset, DokanFileInfo info)
+		{
+			bytesWritten = 0;
+			return DokanResult.Error;
+		}
+	}
 }
--- Gafa/MainWindow.xaml.cs
+++ Gafa/MainWindow.xaml.cs
@@ -12,27 +12,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Gafa.Dokan;
+
 
 namespace Gafa
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
 
-        private void Mount_Click(object sender, RoutedEventArgs e)
-        {
+			MountsList.Instance.Add(new CustomFilesystem(@"U:\", @"C:\Temp"));
+		}
 
-        }
+		private void Mount_Click(object sender, RoutedEventArgs e)
+		{
+			MountsList.Mount();
+		}
 
-        private void Unmount_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-    }
+		private void Unmount_Click(object sender, RoutedEventArgs e)
+		{
+			MountsList.Unmount();
+		}
+	}
 }
