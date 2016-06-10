adc1e837355d3219c7b771cd433c4c5e02122df3
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2016-06-10 23:36:28 +0200

    Add logging capabilities.

---
+++ Gafa/Logger/ILogger.cs
@@ -0,0 +1,20 @@
+﻿using NLog;
+
+namespace Gafa.Logging
+{
+	/// <summary>
+	/// Base class exposing logging capabilities
+	/// </summary>
+	class ILogger
+	{
+		/// <summary>
+		/// Logger
+		/// </summary>
+		protected Logger Log { get; private set; }
+
+		protected ILogger()
+		{
+			Log = LogManager.GetLogger(GetType().FullName);
+		}
+	}
+}
