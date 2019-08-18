9da6e5462593420dddc8c5eee42c03b9118a9109
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2019-08-18 13:00:19 +0200

    simple parsing of arg for path to repo.

--- RepoAsFilystemTranslator/main.c
+++ RepoAsFilystemTranslator/main.c
@@ -32,6 +32,9 @@
 #include <sys/xattr.h>
 #endif
 
+#ifndef S_ISDIR
+#define	S_ISDIR(m)	(((m)&S_IFDIR) == S_IFDIR)
+#endif
 
 
 #define PROGRAM_NAME "RAFT - Repo As Filesystem Translator"
@@ -45,7 +48,7 @@
 #define LOG_INFO(message, ...) printf(message EOL, ##__VA_ARGS__)
 #define LOG_ERROR(message, ...) fprintf(stderr, message EOL, ##__VA_ARGS__)
 
-static int raft_getattr(const char* path, struct stat* stbuf)
+static int raft_getattr(const char* path, struct FUSE_STAT * stbuf)
 {
 	return -1;
 }
@@ -61,7 +64,7 @@ static int raft_readlink(const char* path, char* buf, size_t size)
 }
 
 static int raft_readdir(const char* path, void* buf, fuse_fill_dir_t filler,
-	off_t offset, struct fuse_file_info *fi)
+	FUSE_OFF_T offset, struct fuse_file_info *fi)
 {
 	return -1;
 }
@@ -111,7 +114,7 @@ static int raft_chown(const char* path, uid_t uid, gid_t gid)
 	return -1;
 }
 
-static int raft_truncate(const char* path, off_t size)
+static int raft_truncate(const char* path, FUSE_OFF_T size)
 {
 	return -1;
 }
@@ -128,14 +131,14 @@ static int raft_open(const char* path, struct fuse_file_info* fi)
 	return -1;
 }
 
-static int raft_read(const char* path, char* buf, size_t size, off_t offset,
+static int raft_read(const char* path, char* buf, size_t size, FUSE_OFF_T offset,
 	struct fuse_file_info* fi)
 {
 	return -1;
 }
 
 static int raft_write(const char* path, const char* buf, size_t size,
-	off_t offset, struct fuse_file_info* fi)
+	FUSE_OFF_T offset, struct fuse_file_info* fi)
 {
 	return -1;
 }
@@ -158,7 +161,7 @@ static int raft_fsync(const char* path, int isdatasync,
 
 #ifdef HAVE_POSIX_FALLOCATE
 static int raft_fallocate(const char* path, int mode,
-	off_t offset, off_t length, struct fuse_file_info* fi)
+	FUSE_OFF_T offset, FUSE_OFF_T length, struct fuse_file_info* fi)
 {
 	return -1;
 }
@@ -191,45 +194,95 @@ static int raft_removexattr(const char* path, const char* name)
 
 static struct fuse_operations raft_operations = {
 	.getattr = raft_getattr,
-	.access = raft_access,
 	.readlink = raft_readlink,
-	.readdir = raft_readdir,
+	.getdir = NULL,
 	.mknod = raft_mknod,
 	.mkdir = raft_mkdir,
-	.symlink = raft_symlink,
 	.unlink = raft_unlink,
 	.rmdir = raft_rmdir,
+	.symlink = raft_symlink,
 	.rename = raft_rename,
 	.link = raft_link,
 	.chmod = raft_chmod,
 	.chown = raft_chown,
 	.truncate = raft_truncate,
-#ifdef HAVE_UTIMENSAT
-	.utimens = raft_utimens,
-#endif
+	.utime = NULL,
+
 	.open = raft_open,
 	.read = raft_read,
 	.write = raft_write,
 	.statfs = raft_statfs,
+	.flush = NULL,
 	.release = raft_release,
 	.fsync = raft_fsync,
-#ifdef HAVE_POSIX_FALLOCATE
-	.fallocate = raft_fallocate,
-#endif
 #ifdef HAVE_SETXATTR
 	.setxattr = raft_setxattr,
 	.getxattr = raft_getxattr,
 	.listxattr = raft_listxattr,
 	.removexattr = raft_removexattr,
 #endif
+	.opendir = NULL,
+	.readdir = raft_readdir,
+	.releasedir = NULL,
+	.fsyncdir = NULL,
+	.init = NULL,
+	.destroy = NULL,
+	.access = raft_access,
+	.create = NULL, // if not implemneted mknod -> open
+	.ftruncate = NULL, // if not, truncate is called
+	.fgetattr = NULL,
+	.lock = NULL,
+#ifdef HAVE_UTIMENSAT
+	.utimens = raft_utimens,
+#endif
+	.bmap = NULL,
+#ifdef _WIN32
+	.win_get_attributes = NULL,
+	.win_set_attributes = NULL,
+	.win_set_times = NULL,
+#endif
+	
+	
+#ifdef HAVE_POSIX_FALLOCATE
+	.fallocate = raft_fallocate,
+#endif
+
+};
+
+#define DEFINE_BUFFER(type) \
+	struct buffer_##type { \
+		size_t length; \
+		type * buffer;  \
+    }; \
+	typedef struct buffer_##type buffer_##type##_s
+
+
+#define DEFINE_BUFFER_EMBEDDED(type) \
+	struct buffer_em_##type { \
+		size_t length; \
+		type   buffer[1]; \
+	}; \
+	typedef struct buffer_em_##type buffer_em_##type##_s
+
+DEFINE_BUFFER(char);
+DEFINE_BUFFER_EMBEDDED(char);
+
+struct buffer {
+	size_t length;
+	
 };
 
 struct raft_context {
-	uint32_t counter;
-} g_context;
+	buffer_char_s repository_path;
+};
+
+typedef struct raft_context raft_context_s;
 
 int main(int argc, char* argv[])
 {
+	raft_context_s* pCtx = NULL;
+	int ec = 0;
+
 	LOG_INFO("Starting:      " PROGRAM_NAME );
 	int retVal = git_libgit2_init();
 	if (IS_EC(retVal))
@@ -238,13 +291,51 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
+	if (argc < 2) {
+		LOG_ERROR("Not enough args");
+		ec = -1;
+		goto exit;
+	}
+
+	struct stat repoDirectory;
+	if (stat(argv[1], &repoDirectory) < 0)
+	{
+		LOG_ERROR("Directory %s doesn't exist", argv[1]);
+		ec = -1;
+		goto exit;
+	}
+	else if (!S_ISDIR(repoDirectory.st_mode))
+	{
+		LOG_ERROR("%s is not a directory", argv[1]);
+		ec = -1;
+		goto exit;
+	}
+
+	size_t dirname_len = strlen(argv[1]);
+
+	pCtx = (raft_context_s *)malloc(sizeof(raft_context_s) + dirname_len);
+	if (NULL == pCtx)
+	{
+		LOG_ERROR("Failed allocation of memory for ctx");
+		ec = -1;
+		goto exit;
+	}
+
+	pCtx->repository_path.length = dirname_len;
+	pCtx->repository_path.buffer = (char*)&pCtx[1];
+
 #ifdef linux
 	umask(0);
 #endif
-	fuse_main(argc, argv, &raft_operations, &g_context);
+	fuse_main(argc, argv, &raft_operations, &pCtx);
 
 exit:
 	LOG_INFO("Shutting down: " PROGRAM_NAME);
 	git_libgit2_shutdown();
-	return 0;
+	
+	if (pCtx) {
+		free(pCtx);
+	}
+
+	return ec;
 }
