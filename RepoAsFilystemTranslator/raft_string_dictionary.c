6ffb6e1d35d9a83557cea2228e98a225ffc1dd50
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2019-08-26 22:55:15 +0200

    adjust header - custom compare, key compare, and entry delete functions

--- RepoAsFilystemTranslator/raft_string_dictionary.c
+++ RepoAsFilystemTranslator/raft_string_dictionary.c
@@ -1,2 +1,52 @@
 #include "raft_string_dictionary.h"
 
+int cstring_dictionary_init(cstring_dictionary_s* dict, size_t initial_size, cstring_dictionary_key_compare_fn compare_fn, cstring_dictionary_entry_delete_fn entry_delete_fn)
+{
+	cstring_dictionary_entry_s* entries = (cstring_dictionary_entry_s*)raft_malloc(sizeof(cstring_dictionary_entry_s) * initial_size);
+	if (entries == NULL)
+		return -ENOMEM;
+
+	dict->key_compare_fn = compare_fn;
+	dict->entry_delete_fn = entry_delete_fn;
+	dict->entries = entries;
+	dict->size = initial_size;
+}
+
+void cstring_dictionary_clear(cstring_dictionary_s* dict)
+{
+	if (dict->entries)
+	{
+		if (dict->entry_delete_fn) {
+			while (dict->size > 0) {
+				dict->entry_delete_fn(&dict->entries[dict->size - 1]);
+				--dict->size;
+			}
+		}
+		else {
+			dict->size = 0;
+		}
+
+		raft_free(dict->entries);
+		dict->entries = NULL;
+	}
+}
+
+cstring_dictionary_value_s cstring_dictionary_get(cstring_dictionary_s* dict, cstring_dictionary_key_s* key)
+{
+
+}
+
+int cstring_dictionary_set(cstring_dictionary_s* dict, cstring_dictionary_key_s key, cstring_dictionary_value_s value)
+{
+
+}
+
+void cstring_dictionary_remove(cstring_dictionary_s* dict, cstring_dictionary_key_s key)
+{
+
+}
+
+void cstring_dictionary_foreach(cstring_dictionary_s* dict, cstring_dictionary_entry_fn function)
+{
+
+}
--- RepoAsFilystemTranslator/raft_structures_dictionary.h
+++ RepoAsFilystemTranslator/raft_structures_dictionary.h
@@ -4,22 +4,32 @@
 #include "raft_base.h"
 
 #define DECLARE_DICTIONARY(key_type, value_type, name) \
+typedef key_type name##_key_s;\
+typedef value_type name##_value_s;\
+typedef int (*name##_compare_fn)(name##_entry_s* a, name##_entry_s* b);\
+typedef int (*name##_key_compare_fn)(name##_key_s* a, name##_key_s* b);\
+typedef void (*name##_entry_delete_fn)(name##_entry_s* entry);\
+\
 typedef struct name##_entry { \
-	key_type key; \
-	value_type value; \
+	name##_key_s key; \
+	name##_value_s value; \
 } name##_entry_s; \
 \
 typedef struct name {\
-    name##_entry_s* entries; \
+	name##_key_compare_fn key_compare_fn; \
+	name##_entry_delete_fn entry_delete_fn; \
+	size_t size; \
+	name##_entry_s* entries; \
 } name##_s; \
 \
+int name##_init(name##_s *dict, size_t initial_size, name##_key_compare_fn compare_fn, name##_entry_delete_fn entry_delete_fn);\
 void name##_clear(name##_s *dict); \
-value_type name##_get(name##_s *dict, key_type key); \
-int name##_set(name##_s *dict, key_type key, value_type value); \
-void name##_remove(name##_s *dict, key_type key); \
+name##_value_s name##_get(name##_s *dict, name##_key_s* key); \
+int name##_set(name##_s *dict, name##_key_s key, name##_value_s value); \
+void name##_remove(name##_s *dict, name##_key_s key); \
 \
 typedef int (*name##_entry_fn)(name##_entry_s* entry); \
-void name##_foreach(name##_entry_fn function)
+void name##_foreach(name##_s *dict, name##_entry_fn function)
 
 
 
