83e082fdd1e249c06cb532b5032f66b202b4b37f
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2019-08-26 22:17:49 +0200

    c style declare of dictionary with functions

---
+++ RepoAsFilystemTranslator/raft_structures_dictionary.h
@@ -0,0 +1,25 @@
+#ifndef RAFT_STRUCTURES_DICTIONARY_H
+#define RAFT_STRUCTURES_DICTIONARY_H
+
+#define DECLARE_DICTIONARY(key_type, value_type, name) \
+typedef struct name##_entry { \
+	key_type key; \
+	value_type value; \
+} name##_entry_s; \
+\
+typedef struct name {\
+    name##_entry_s* entries; \
+} name##_s; \
+\
+void name##_clear(name##_s *dict); \
+value_type name##_get(name##_s *dict, key_type key); \
+int name##_set(name##_s *dict, key_type key, value_type value); \
+void name##_remove(name##_s *dict, key_type key); \
+\
+typedef int (*name##_entry_fn)(name##_entry_s* entry); \
+void name##_foreach(name##_entry_fn function)
+
+
+
+
+#endif /* RAFT_STRUCTURES_DICTIONARY_H */
